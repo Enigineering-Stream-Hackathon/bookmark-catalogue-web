e93d8b73afc30557613e6306e8bffe372b210060
sOubHik <soubhik93@gmail.com>
2020-09-18 11:06:04 +0530

    Local setup and Readme

--- src/bookmark-catalogue/components/constants.js
+++ src/bookmark-catalogue/components/constants.js
@@ -1,3 +1,10 @@
-export const BOOKMARK_CATALOGUE = 'https://bookmark-catalogue.herokuapp.com/'
+export const BOOKMARK_CATALOGUE = 'https://bookmark-catalogue.herokuapp.com'
 export const USER_MANAGEMENT = 'https://user-management-bs.herokuapp.com'
 export const BOOKMARK_CATALOGUE_WEB = 'https://bookmark-catalogue-web.herokuapp.com'
+
+
+/*
+//For local set up
+export const BOOKMARK_CATALOGUE = 'http://localhost:8085'
+export const USER_MANAGEMENT = 'http://localhost:8082'
+export const BOOKMARK_CATALOGUE_WEB = 'http://localhost:3000'*/
